a93105766a4ea6d0d7ded8d332c5dd72dd391fa5
Ahmed <ahmedmo501296@gmail.com>
2024-03-09 21:00:44 +0000

    add code, files, done  city quiz page and more

---
+++ Server-Side/database.js
@@ -0,0 +1,27 @@
+const { error } = require('console');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'quiz'
+  });
+// connect to mysql server
+  db.connect((err) => {
+    if (err){
+        console.err("conntection error: " + err)
+    }
+    console.log("sql conection up")
+  });
+
+  //query to text if the myserver is working and can put data.
+  const sql =' select CountryName, cityname from Country  INNER JOIN City on City.cityID  = Country.CountryID;'
+
+  db.query(sql, (error,results) => {
+    if(error){
+        console.error("query error" + error)
+    }
+
+    console.log(results)
+  });
