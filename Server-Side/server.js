f0fe7fee80125229a4bd90a9c91222f4e1cfb1eb
Ahmed <ahmedmo501296@gmail.com>
2024-04-10 11:43:07 +0100

    fixed api

--- Server-Side/server.js
+++ Server-Side/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
+const db = require('./database');
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -12,6 +12,26 @@ app.listen(port, () => {
 ///calls api from staitic page
 const api = require('./api');
 app.use('/api', api);
+app.get("/cityQ", (req, res) => {
+  const sql = 'SELECT c.CountryName, ci.cityname FROM Country c JOIN City ci ON ci.cityID = c.CountryID ORDER BY RAND() LIMIT 4;';
+
+  db.query(sql, (error, results) => {
+    if (error) {
+      console.error("Query error: " + error);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      console.log("Results:", results);
+      const citydata = results.map(result => ({
+        CountryName: result.CountryName,
+        Cityname: result.cityname
+      }));
+      console.log("City Data:", citydata);
+      res.json(citydata);
+    }
+  });
+});
+
+
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '..', 'Client-Side', 'Public')));
